f90969cca469ea16ebd29036b6e4937841e6e2d8
TheGoodCoder12 <satyam.strange93@gmail.com>
2026-09-12 14:06:21 +0530

    project strutured thought and BE tested

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Ivy Homes
+      </h1>
+    </div>
+  )
+}
+
+export default App
